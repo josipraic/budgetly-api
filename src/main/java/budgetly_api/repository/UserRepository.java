829731f6b9014b7f523b34ac8689ed14682b922d
Josip Raic <raicjosip39@gmail.com>
2026-05-20 19:31:47 +0200

    add user entity

---
+++ src/main/java/budgetly_api/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package budgetly_api.repository;
+
+import budgetly_api.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+
+    boolean existByEmail(String email);
+}
